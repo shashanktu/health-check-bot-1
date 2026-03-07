adc4045c8e7d203584168f0627882cca1ad6d48f
shashanktu <133744620+shashanktu@users.noreply.github.com>
2026-03-07 10:47:08 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,8 @@
-#!/bin/bash
 
-echo "Installing nginx..."
-apt-get update
-apt-get install -y nginx
 
 echo "Starting FastAPI..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 echo "Starting Streamlit..."
-streamlit run health.py --server.port 8501 --server.address 0.0.0.0 &
+streamlit run streamlit_app.py --server.port $PORT --server.address 0.0.0.0
 
-echo "Starting nginx..."
-nginx -c $(pwd)/nginx.conf -g "daemon off;"
