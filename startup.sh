acacbc3b1347bf196aca917da2f37de20c1944f4
shashanktu <133744620+shashanktu@users.noreply.github.com>
2026-03-09 13:56:36 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Starting FastAPI..."
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+uvicorn app.main:app --host 0.0.0.0 --port 8502 &
 
 echo "Starting Streamlit..."
 streamlit run health.py --server.port $PORT --server.address 0.0.0.0
